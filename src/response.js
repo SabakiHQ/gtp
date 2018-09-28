1e8b5d54655331ccfafb6ddbfcc876eb28ad2100
Yichuan Shen <yichuan.shen@sap.com>
2018-09-28 16:13:16 +0200

    Fix id error when 2nd char of response is not a space

--- src/response.js
+++ src/response.js
@@ -2,10 +2,10 @@ exports.fromString = function(input) {
     input = input.replace(/\t/g, ' ').trim()
 
     let error = input[0] !== '='
-    let hasId = input.length >= 2 && input[1] !== ' '
+    let hasId = input.length >= 2 && input[1].match(/\d/) != null
 
     input = input.slice(1)
-    let id = hasId ? +input.split(' ')[0] : null
+    let id = hasId ? +input.split(/\s/)[0] : null
 
     if (hasId) input = input.slice((id + '').length)
     return {id, content: input.trim(), error}
